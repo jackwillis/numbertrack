6403c0e12505f508bae492a0a8f8db34aa1f3372
Jack Willis <jack@attac.us>
2014-12-11 00:06:42 -0600

    basic app footprint

--- numbertrack.py
+++ numbertrack.py
@@ -18,26 +18,105 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from tkinter import *
+from tkinter.ttk import *
+
+import shelve
+import datetime
+
+NUMBERTRACK_VERSION = "0.0.1"
 
 class NumberTrack(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
 
         self.parent = parent
-        self.parent.title("numbertrack")
+        self.parent.title("Numbertrack v%s" % NUMBERTRACK_VERSION)
 
-        self.pack(fill=BOTH, expand=1)
+        self.numberstore = NumberStore("numbers.db")
 
-        quitButton = Button(self, text="Add number", command=self.addNumber)
-        quitButton.place(x=50, y=50)
+        Style().configure("TButton", padding=6, relief="flat", background="#ccc")
 
-    def addNumber(self):
-        print("test")
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, weight=1)
+        self.rowconfigure(1, weight=1)
 
+        self.modifyEntry = Entry(self)
+        self.modifyEntry.grid(row=0, column=0, columnspan=2, sticky=E+W)
+        self.modifyEntry.insert(0, "555-1212")
+
+        touchNumberButton = Button(self, text="Touch number", command=self.touchNumber)
+        touchNumberButton.grid(row=0, column=2, sticky=E+W)
+
+        deleteNumberButton = Button(self, text="Delete number", command=self.deleteNumber)
+        deleteNumberButton.grid(row=0, column=3, sticky=E+W)
+
+        self.numbersBox = Listbox(self)
+        self.numbersBox.grid(row=1, column=0, columnspan=2, sticky=N+S+E+W)
+
+        fakeNumberList = ['555-5551','212-2231','123-5678','727-8383','339-4422']
+
+        for num in fakeNumberList:
+            self.numbersBox.insert(END, num)
+
+        self.infoText = Text(self)
+        self.infoText.grid(row=1, column=2, columnspan=2, sticky=N+S+E+W)
+
+        self.showAllButton = Button(self, text="Show All", command=self.showAll)
+        self.showAllButton.grid(row=2, column=0, sticky=E+W)
+
+        self.searchButton = Button(self, text="Search", command=self.search)
+        self.searchButton.grid(row=2, column=1, sticky=E+W)
+
+        self.searchEntry = Entry(self)
+        self.searchEntry.grid(row=2, column=2, columnspan=2, sticky=E+W)
+
+
+        self.pack()
+
+    def touchNumber(self):
+        newNumber = self.modifyEntry.get()
+        self.numberstore.touchNumber(newNumber)
+
+    def deleteNumber(self):
+        newNumber = self.modifyEntry.get()
+        self.numberstore.deleteNumber(newNumber)
+
+    def showAll(self):
+        print()
+
+    def search(self):
+        print()
+
+    def searchTags(self):
+        print()
+
+class NumberStore():
+    def __init__(self, filename):
+        self.db = shelve.open(filename, writeback=True)
+
+        if not 'numbers' in self.db:
+            self.db['numbers'] = {}
+
+    def initNumber(self, number):
+        if not number in self.db['numbers']:
+            self.db['numbers'][number] = {'accesses': []}
+
+    def touchNumber(self, number):
+        self.initNumber(number)
+        self.db['numbers'][number]['accesses'].append(datetime.datetime.now())
+        print(self.db.__dict__['cache'])
+
+    def deleteNumber(self, number):
+        if number in self.db['numbers']:
+            del self.db['numbers'][number]
+
+        print(self.db.__dict__['cache'])
+
+    def close(self):
+        db.close()
 
 def main():
     root = Tk()
-    root.geometry("400x200")
     app = NumberTrack(root)
     root.mainloop()
 
