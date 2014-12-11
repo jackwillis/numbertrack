66625decc406cd0bf108862274480da9bde3ef66
Jack Willis <jack@attac.us>
2014-12-11 13:02:03 -0600

    number viewing functionality

--- numbertrack.py
+++ numbertrack.py
@@ -38,32 +38,30 @@ class NumberTrack(Frame):
 
         self.columnconfigure(2, weight=1)
         self.columnconfigure(3, weight=1)
-        self.columnconfigure(4, weight=1)
         self.rowconfigure(1, weight=1)
 
         self.modifyEntry = Entry(self)
         self.modifyEntry.grid(row=0, column=0, columnspan=2, sticky=E+W)
-        self.modifyEntry.insert(0, "555-1212")
 
         touchNumberButton = Button(self, text="Touch", command=self.touchNumber)
         touchNumberButton.grid(row=0, column=2, sticky=E+W)
 
-        touchNumberButton = Button(self, text="Update", command=self.updateInfo)
-        touchNumberButton.grid(row=0, column=3, sticky=E+W)
-
         deleteNumberButton = Button(self, text="Delete", command=self.deleteNumber)
-        deleteNumberButton.grid(row=0, column=4, sticky=E+W)
+        deleteNumberButton.grid(row=0, column=3, sticky=E+W)
 
         self.numbersBox = Listbox(self)
         self.numbersBox.grid(row=1, column=0, columnspan=2, sticky=N+S+E+W)
+        self.numbersBox.bind('<<ListboxSelect>>', self.selectNumber)
 
         fakeNumberList = ['555-5551','212-2231','123-5678','727-8383','339-4422']
-
         for num in fakeNumberList:
+            self.numberstore.touchNumber(num)
+
+        for num in self.numberstore.getNumbers():
             self.numbersBox.insert(END, num)
 
         self.infoText = Text(self)
-        self.infoText.grid(row=1, column=2, columnspan=3, sticky=N+S+E+W)
+        self.infoText.grid(row=1, column=2, columnspan=2, sticky=N+S+E+W)
 
         self.showAllButton = Button(self, text="Show All", command=self.showAll)
         self.showAllButton.grid(row=2, column=0, sticky=E+W)
@@ -72,8 +70,7 @@ class NumberTrack(Frame):
         self.searchButton.grid(row=2, column=1, sticky=E+W)
 
         self.searchEntry = Entry(self)
-        self.searchEntry.grid(row=2, column=2, columnspan=3, sticky=E+W)
-
+        self.searchEntry.grid(row=2, column=2, columnspan=2, sticky=E+W)
 
         self.pack()
 
@@ -81,22 +78,42 @@ class NumberTrack(Frame):
         number = self.modifyEntry.get()
         self.numberstore.touchNumber(number)
 
-    def updateInfo(self):
+    def saveInfo(self):
         number = self.modifyEntry.get()
         info = self.infoText.get('1.0', END)
-        self.numberstore.updateInfo(number, info)
+        self.numberstore.setInfo(number, info.strip())
+
+        print("set info of %s to %s" % (number, info))
 
     def deleteNumber(self):
         number = self.modifyEntry.get()
         self.numberstore.deleteNumber(number)
 
-    def showAll(self):
+    # called when focus is called to an number in the listbox
+    def selectNumber(self, evt):
+        self.saveInfo()
+
+        # get selected number
+        w = evt.widget
+        index = int(w.curselection()[0])
+        number = w.get(index)
+
+        # display the number in the top entry box
+        self.modifyEntry.delete(0, END)
+        self.modifyEntry.insert(0, number)
+
+        # display the number's info in the text box
+        info = self.numberstore.getInfo(number)
+        self.infoText.delete("1.0", END)
+        self.infoText.insert(END, (info or ""))
+
+    def showAll(self, evt):
         print()
 
-    def search(self):
+    def search(self, evt):
         print()
 
-    def searchTags(self):
+    def searchTags(self, evt):
         print()
 
 class NumberStore():
@@ -108,7 +125,7 @@ class NumberStore():
 
     def initNumber(self, number):
         if not number in self.db['numbers']:
-            self.db['numbers'][number] = {'accesses': []}
+            self.db['numbers'][number] = {'accesses': [], 'info': '#yolo'}
 
     def touchNumber(self, number):
         self.initNumber(number)
@@ -116,7 +133,24 @@ class NumberStore():
 
         print(self.db.__dict__['cache'])
 
-    def updateInfo(self, number, info):
+    def getNumbers(self):
+        return self.db['numbers']
+
+    def getAccesses(self, number):
+        if not number in self.db['numbers']:
+            return None
+        if not 'info' in self.db['numbers'][number]:
+            return None
+
+        return self.db['numbers'][number]['info']
+
+    def getInfo(self, number):
+        if not number in self.db['numbers']:
+            return None
+
+        return self.db['numbers'][number]['info']
+
+    def setInfo(self, number, info):
         self.initNumber(number)
         self.db['numbers'][number]['info'] = info
 
