798707daeb81cdca09e156a0cd1f54d67fc32226
Jack Willis <jack@attac.us>
2014-12-11 20:23:30 -0600

    Switch from shelve to tinydb

--- numberstore.py
+++ numberstore.py
@@ -7,54 +7,53 @@
 # Licensed under GNU General Public License 3.0 or later.
 # Some rights reserved. See COPYING.
 
-import shelve
+from tinydb import TinyDB, where
 import datetime
 
 class NumberStore():
     def __init__(self, filename):
-        self.db = shelve.open(filename, writeback=True)
-
-        if not 'numbers' in self.db:
-            self.db['numbers'] = {}
+        self.db = TinyDB(filename)
 
     def initNumber(self, number):
-        if not number in self.db['numbers']:
-            self.db['numbers'][number] = {'accesses': [], 'info': '#yolo'}
+        if not self.getNumberDict(number):
+            self.db.insert({'number': number, 'accesses': [], 'info': '#yolo'})
 
     def touchNumber(self, number):
         self.initNumber(number)
-        self.db['numbers'][number]['accesses'].append(datetime.datetime.now())
 
-        print(self.db.__dict__['cache'])
+        #print(self.getNumberDict(number))
+        #accesses = self.getNumberDict(number)['accesses'].append(datetime.datetime.now())
 
-    def getNumbers(self):
-        return self.db['numbers']
+        #self.db.update({'accesses': accesses}, where('number') == number)
+
+    def getNumberDict(self, number):
+        return self.db.get(where('number') == number)
+
+    def getNumberList(self):
+        return (entry['number'] for entry in self.db.all())
 
     def getAccesses(self, number):
-        if not number in self.db['numbers']:
-            return None
-        if not 'info' in self.db['numbers'][number]:
-            return None
-
-        return self.db['numbers'][number]['info']
+        # if not number in self.db['numbers']:
+        #     return None
+        # if not 'info' in self.db['numbers'][number]:
+        #     return None
+        #
+        # return self.db['numbers'][number]['info']
+        return []
 
     def getInfo(self, number):
-        if not number in self.db['numbers']:
-            return None
-
-        return self.db['numbers'][number]['info']
+        return self.getNumberDict(number)['info']
 
     def setInfo(self, number, info):
         self.initNumber(number)
-        self.db['numbers'][number]['info'] = info
+        self.db.update({'info': info}, where('number') == number)
 
-        print(self.db.__dict__['cache'])
+        print(self.db.all())
 
     def deleteNumber(self, number):
-        if number in self.db['numbers']:
-            del self.db['numbers'][number]
+        self.db.remove(where('number') == number)
 
-        print(self.db.__dict__['cache'])
+        print(self.db.all())
 
     def close(self):
-        db.close()
+        self.db.close()
--- setup.py
+++ setup.py
@@ -5,22 +5,7 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-packages = []
-
-# need a db module for shelve
-for dbmodule in ['dbhash', 'gdbm', 'dbm', 'dumbdbm']:
-    try:
-        __import__(dbmodule)
-    except ImportError:
-        pass
-    else:
-        packages.append(dbmodule)
-
 cx_Freeze.setup( name = "Numbertrack",
                  version = "0.0.1",
                  description = "Numbertrack",
-                 options = {
-                        "packages": packages
-                    }
-                 },
                  executables = [cx_Freeze.Executable("numbertrack.py", base=base)] )
